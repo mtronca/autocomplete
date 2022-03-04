c2f858e40724530d89c34c0578a009599819abd0
mtronca <murilo.tronca@hotmail.com>
2022-03-03 22:33:57 -0300

    build: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+import './global.css';
+
+const App = () => {
+	return (
+		<h1>Autocomplete</h1>
+	)
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const webpack = require("webpack");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "production",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ], 
+  },  
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "dist/index.html"
+    }),
+  ],
+  resolve: {
+    enforceExtension: false,
+    modules: [
+        path.join(__dirname, 'node_modules')
+    ],
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
+}
