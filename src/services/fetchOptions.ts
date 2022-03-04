0227a3d26a85ffc9f8179662ea7ff40f48c70737
mtronca <murilo.tronca@hotmail.com>
2022-03-03 23:27:59 -0300

    feat: fetch options API call

--- src/services/fetchOptions.ts
+++ src/services/fetchOptions.ts
@@ -1,14 +1,28 @@
-const fetchOptions = async (value: string) => {
-  const options = [
-    'murilo',
-    'peter',
-    'fred',
-    'joe',
-  ];
-  
-  return new Promise(resolve => {
-    setTimeout(() => resolve(options.filter(option => option.includes(value))), 200)
+const fetchOptions = async (value: string, limit = 5) => {
+  const arrayLimit = limit;
+
+  const url = `https://hotels4.p.rapidapi.com/locations/v2/search?query=${value}&local=en_US&currency=USD`;
+  const options = {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "hotels4.p.rapidapi.com",
+      "x-rapidapi-key": "gXDfR45LI6msh7UbwzoT87tAuZcRp1K7YyijsnJTlAsVKxcjdv"
+    }
+  };
+
+  return fetch(url, options)
+  .then((response: any) => {
+    return response.json();
   })
+  .then(data => {
+    const cities = data?.suggestions.find(s => s.group === 'CITY_GROUP')?.entities || [];
+    const filteredArr = cities.map((item: Record<string, string>) => item.name).slice(0, arrayLimit) || [];
+    return filteredArr;
+  })
+  .catch(err => {
+    console.error(err);
+    return [];
+  });
 }
 
 export default fetchOptions;
